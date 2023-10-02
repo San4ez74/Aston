9a8e653b05be033e58c4b7d2ec400edff6d5b336
San4ez74 <San4ez74@yandex.ru>
2023-10-02 20:37:52 +0500

    Aston_Gabiets_Java

--- Task_1/src/Main.java
+++ Task_1/src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Введите целое число: ");
         Scanner scanner = new Scanner(System.in);
         int enteredNumber = scanner.nextInt();
         if (enteredNumber > 7){
